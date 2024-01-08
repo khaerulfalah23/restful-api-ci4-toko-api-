f30ea5ea0bc245917ef5c904ddeb81bbdef7916e
khaerulfalah23 <khaerulfalah23@gmail.com>
2024-01-08 10:07:54 +0700

    feat: crud produk

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,10 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->post('/registrasi', 'RegistrasiController::registrasi');
 $routes->post('/login', 'LoginController::login');
+$routes->group('produk', function ($routes) {
+  $routes->post('/', 'ProdukController::create');
+  $routes->get('/', 'ProdukController::list');
+  $routes->get('(:segment)', 'ProdukController::detail/$1');
+  $routes->put('(:segment)', 'ProdukController::ubah/$1');
+  $routes->delete('(:segment)', 'ProdukController::hapus/$1');
+});
---
+++ app/Controllers/ProdukController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\MProduk;
+
+class ProdukController extends RestfulController
+{
+
+  public function create()
+  {
+    $data = [
+      'kode_produk' => $this->request->getVar('kode_produk'),
+      'nama_produk' => $this->request->getVar('nama_produk'),
+      'harga' => $this->request->getVar('harga')
+    ];
+    $model = new MProduk();
+    $model->insert($data);
+    $produk = $model->find($model->getInsertID());
+    return $this->responseHasil(200, true, $produk);
+  }
+
+  public function list()
+  {
+    $model = new MProduk();
+    $produk = $model->findAll();
+    return $this->responseHasil(200, true, $produk);
+  }
+
+  public function detail($id)
+  {
+    $model = new MProduk();
+    $produk = $model->find($id);
+    return $this->responseHasil(200, true, $produk);
+  }
+
+  public function ubah($id)
+  {
+    $data = [
+      'kode_produk' => $this->request->getVar('kode_produk'),
+      'nama_produk' => $this->request->getVar('nama_produk'),
+      'harga' => $this->request->getVar('harga')
+    ];
+    $model = new MProduk();
+    $model->update($id, $data);
+    $produk = $model->find($id);
+    return $this->responseHasil(200, true, $produk);
+  }
+
+  public function hapus($id)
+  {
+    $model = new MProduk();
+    $produk = $model->delete($id);
+    return $this->responseHasil(200, true, $produk);
+  }
+}
